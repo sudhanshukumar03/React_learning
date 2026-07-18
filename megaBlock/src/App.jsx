ea2016f7a9baa2b0c5b7414c2549a35420c91712
sudhanshu <sksingh032003@gmail.com>
2026-07-19 04:43:32 +0530

    learnign appwrite config

---
+++ megaBlock/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  
+
+  return (
+    <>
+      <h1>Hi I'm Sudhanshu Singh</h1>
+    </>
+  )
+}
+
+export default App
