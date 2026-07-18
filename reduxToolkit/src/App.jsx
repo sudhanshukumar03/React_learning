f847a00affc3eafa70e00ecc037754e8e9163383
sudhanshu <sksingh032003@gmail.com>
2026-07-18 23:49:04 +0530

    completed redux toolkit

--- reduxToolkit/src/App.jsx
+++ reduxToolkit/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+import Todos from './components/Todo'
 
 function App() {
- 
   return (
     <>
-    
+      <h1>Learn About Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
   )
 }
---
+++ reduxToolkit/src/components/Todo.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+function Todos() {
+    const todos = useSelector((state) => state.todos)
+    const dispatch = useDispatch()
+
+    return (
+        <>
+            <div>Todos</div>
+
+            <ul className="list-none">
+                {todos.map((todo) => (
+                    <li
+                        key={todo.id}
+                        className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+                    >
+                        <div className="text-white">{todo.text}</div>
+
+                        <button
+                            onClick={() => dispatch(removeTodo(todo.id))}
+                            className="text-white bg-red-500 border-0 py-1 px-4 hover:bg-red-600 rounded"
+                        >
+                            Delete
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default Todos
