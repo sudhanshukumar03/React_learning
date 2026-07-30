f679d375c15202d7b58e62b89e02a35c90d37d20
sudhanshu <sksingh032003@gmail.com>
2026-07-30 18:40:31 +0530

    Add initial Appwrite configuration imports

---
+++ megaBlock/src/appwrite/config.js
@@ -0,0 +1,60 @@
+import conf  from '../conf.js';
+import {client, ID,Databases,Storage,Query} from 'appwrite';
+
+export class service{
+    client = new client();  
+    Databases;
+    Storage;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.Databases=new Databases(this.client);
+        this.Storage=new Storage(this.client);
+    }
+    async createPost({title,slug,content,featureImage,status,userId}){
+        try {
+            return await this.Databases.createDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,{
+                title,
+                content,
+                featureImage,
+                status,
+                userId})
+        } catch (error) {
+            console.log("Appwrite service :: createDocument :: error",Error);  
+            
+        }
+    }
+    async updateDocument(slug,{title,content,featureImage,status,userId}){
+        try {
+            return await this.Databases.updateDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,{
+                title,
+                content,
+                featureImage,
+                status
+                
+            })
+        } catch (error) {
+            console.log("Appwrite service :: updateDocument :: error",Error);
+        }
+    }
+    async deletePost(slug){
+        try {
+            return await this.Databases.deletePost(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true
+        } 
+        catch (error) 
+        {
+            console.log("Appwrite service :: deletePost :: error",Error);
+        }   
+    }
+        
+}
+    
+const Service = new service();
+export default Service;
