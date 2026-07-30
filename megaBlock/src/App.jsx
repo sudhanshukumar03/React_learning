f4b0d3409cff6f72ae61a662e362f60fa6106024
sudhanshu <sksingh032003@gmail.com>
2026-07-31 05:09:56 +0530

    feat: add Tailwind CSS support and update authSlice
    - Added @tailwindcss/vite and tailwindcss dependencies to package.json
    - Updated vite.config.js to include Tailwind CSS plugin
    - Enhanced authSlice with logout action
    - Created Footer and Header components
    - Updated index.js to export new components

--- megaBlock/src/App.jsx
+++ megaBlock/src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-
+import { use, useState } from 'react'
+import{useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading, setLoading] = useState(true)//true cause useEffect will run after the first render and we want to show the loading screen before the first render is complete,so we set loading to true initially and then we set it to false after the first render is complete
+  const dispatch=useDispatch()
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))//finally is used to set loading to false after the first render is complete,so that we can show the main content of the app after the first render is complete
+  },[])
+return !loading? (
+     <div> </div>
+):null
 
-  return (
-    <>
-      <h1>Hi I'm Sudhanshu Singh</h1>
-    </>
-  )
 }
 
 export default App
--- megaBlock/src/store/authSlice.js
+++ megaBlock/src/store/authSlice.js
@@ -12,7 +12,27 @@ const authSlice = createSlice({
         login:(state,action)=>{
             state.status=true;
             state.userData=action.payload;
+        },
+        logout:(state)=>{   
+            state.status=false;
+            state.userData=null;
         }
     }
+   
+
+    })
+     //definition of post is to be done in the postSlice.js file because we are going to create a separate slice for post,why do we need to create a separate slice for post because we are going to create a separate component for post and we are going to use the postSlice.js file to manage the state of the post component,so we need to create a separate slice for post
+    const postSlice = createSlice({
+        name:"post",
+        initialState:initialState,
+        reducers:{
+            posts: (state, action) => {
+                state.status = true;
+                state.posts = action.payload;
+                
+            }
+        }
+
 });
+export const {login,logout}=authSlice.actions;
 export default authSlice.reducer;
