9ff87c57cb269f659b74c78a870c4f336ab8230f
sudhanshu <sksingh032003@gmail.com>
2026-07-31 05:55:19 +0530

    feat: integrate Header and Footer components, update App structure, and configure Redux store

--- megaBlock/src/App.jsx
+++ megaBlock/src/App.jsx
@@ -1,8 +1,9 @@
-import { use, useState } from 'react'
+import { useState, useEffect } from "react";
 import{useDispatch} from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
 import {login,logout} from './store/authSlice'
+import { Header, Footer } from "./components";
 
 function App() {
   const [loading, setLoading] = useState(true)//true cause useEffect will run after the first render and we want to show the loading screen before the first render is complete,so we set loading to true initially and then we set it to false after the first render is complete
@@ -11,7 +12,7 @@ function App() {
     authService.getCurrentUser()
     .then((userData)=>{
       if(userData){
-        dispatch(login({userData}))
+        dispatch(login(userData))
       }
       else{
         dispatch(logout())
@@ -20,9 +21,18 @@ function App() {
     .finally(()=>setLoading(false))//finally is used to set loading to false after the first render is complete,so that we can show the main content of the app after the first render is complete
   },[])
 return !loading ? (
-  <div className="text-2xl font-bold text-green-500">
-    Tailwind is Working 🚀
-  </div>
+  <div>
+   <Header />
+
+    <main className="min-h-screen flex items-center justify-center">
+      <h2 className="text-3xl font-bold text-green-500">
+        Tailwind is Working 🚀
+      </h2>
+    </main>
+
+    <Footer />
+    </div>
+  
 ) : null;
 
 }
--- megaBlock/src/store/store.js
+++ megaBlock/src/store/store.js
@@ -1,6 +1,10 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 
-const store=configureStore({
-    reducer:{}
-}); 
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+  },
+});
+
 export default store;
